3995fa5cd38bc7ca360486cb38c784447c55b6b5
ivandorte <edgar_allan_poe88@hotmail.it>
2023-06-16 13:50:34 +0200

    added spyndex integration
    Signed-off-by: ivandorte <edgar_allan_poe88@hotmail.it>

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import holoviews as hv
 import panel as pn
-import xarray as xr
+# import xarray as xr
 import json
-from odc.stac import stac_load
+# from odc.stac import stac_load
 import pystac
 from pystac_client.client import Client
 
 from modules.chat_agent import component as chat_agent
-from modules.constants import S2_BAND_COMB, S2_SPINDICES
 from modules.image_plots import (
     # plot_s2_band_comb,
     plot_s2_spindex,
     plot_true_color_image,
 )
 from modules.image_statistics import HIST_PLACEHOLDER, plot_s2_spindex_hist
+from modules.spyndex_utils import get_s2_indices
+# from holoviews.plotting.util import process_cmap
 
 # Load the floatpanel & terminal extension
 pn.extension("floatpanel")
@@ -22,7 +23,9 @@ pn.extension('terminal')
 # Disable webgl: https://github.com/holoviz/panel/issues/4855
 hv.renderer("bokeh").webgl = False
 
+
 client = Client.open('https://earth-search.aws.element84.com/v1/')
+
 
 def create_s2_dashboard():
     """
@@ -42,20 +45,20 @@ def create_s2_dashboard():
     time_opts = dict(zip(time_date, time_date))
     time_select = pn.widgets.DatePicker(
         name="Date",
-        value=time_date[0], 
-        start=time_date[-1], 
-        end=time_date[0], 
+        value=time_date[0],
+        start=time_date[-1],
+        end=time_date[0],
         enabled_dates=time_date,
         description="Select the date for plotting."
         )
 
-    # Sentinel-2 spectral indices ToogleGroup 
+    # Sentinel-2 spectral indices ToogleGroup
     # TODO: Switch AutocompleteInput values/ spectral index calc to spyndex
 
     s2_spindices_ac = pn.widgets.AutocompleteInput(
-        name='Spectral Index', 
+        name='Spectral Index',
         restrict=True,
-        options=list(S2_SPINDICES.keys()),
+        options=get_s2_indices(),
         value='NDVI',
         description="Select the [index](https://davemlz-espectro-espectro-91350i.streamlit.app/) for overlay plot."
         )
@@ -67,13 +70,14 @@ def create_s2_dashboard():
 
     # Resolution slider
     res_select = pn.widgets.IntInput(
-        name="Resolution", 
+        name="Resolution",
         start=20, end=2500, step=50, value=250,
         description="Select the display resolution in meters.")
 
-    # Mask clouds Switch
-    clm_title = pn.widgets.StaticText(name="", value="Mask clouds?")
-    clm_switch = pn.widgets.Switch(name="Switch")
+    # Colormap selector (e. g Diverging) - This can be improved...
+    diverging_cmap = hv.plotting.util.list_cmaps(records=True, category="Diverging", reverse=False)
+    diverging_cmap = list(set([div_.name for div_ in diverging_cmap]))
+    cmap_select = pn.widgets.Select(name="Colormap", options=diverging_cmap, value="RdYlGn")
 
     # TODO: could these be merged into a single function that returns the slider plot?
     # Or returns 2 hv plots that are bound w/ Swipe below?
@@ -82,7 +86,6 @@ def create_s2_dashboard():
         plot_true_color_image,
         items=items,
         time=time_select,
-        mask_clouds=clm_switch,
         resolution=res_select
     )
 
@@ -91,19 +94,19 @@ def create_s2_dashboard():
         items=items,
         time=time_select,
         s2_spindex=s2_spindices_ac,
-        mask_clouds=clm_switch,
-        resolution=res_select
+        resolution=res_select,
+        cmap=cmap_select
     )
 
     # Use the Swipe tool to compare the spectral index with the true color image
     spindex_truecolor_swipe = pn.Swipe(
-        pn.pane.HoloViews(s2_true_color_bind, sizing_mode='stretch_both'), 
+        pn.pane.HoloViews(s2_true_color_bind, sizing_mode='stretch_both'),
         pn.pane.HoloViews(s2_spindex_bind, sizing_mode='stretch_both')
         )
 
     # Create the main layout
     main_layout = pn.Row(
-        pn.Column(HIST_PLACEHOLDER, spindex_truecolor_swipe)#, show_hist_bt),
+        pn.Column(HIST_PLACEHOLDER, spindex_truecolor_swipe)  # , show_hist_bt),
     )
 
     # Create the dashboard and turn into a deployable application
@@ -116,6 +119,7 @@ def create_s2_dashboard():
             time_select,
             s2_spindices_ac,
             res_select,
+            cmap_select
             # clm_title,
             # clm_switch,
         ],
--- modules/image_plots.py
+++ modules/image_plots.py
@@ -2,7 +2,7 @@ import datetime
 import holoviews as hv
 import numpy as np
 import panel as pn
-from bokeh.models import CustomJSHover, HoverTool, WheelZoomTool
+from bokeh.models import HoverTool, WheelZoomTool
 from modules.image_processing import s2_contrast_stretch, s2_image_to_uint8
 from modules.image_statistics import enable_hist_refresh_bt
 import spyndex
@@ -12,25 +12,22 @@ import rasterio as rio
 from rioxarray.merge import merge_arrays
 from rasterio.session import AWSSession
 
-from modules.constants import S2_SPINDICES
+from modules.spyndex_utils import compute_index, get_index_props
 
-# This function hide the tooltip when the pixel value is NaN
-HIDE_NAN_HOVTOOL = CustomJSHover(
-    code="""
-    var value;
-    var tooltips = document.getElementsByClassName("bk-tooltip");
-    if (isNaN(value)) {
-        tooltips[0].hidden=true;
-    } else {
-        tooltips[0].hidden=false;
-    }
-        return value;
-    """
-)
+hv.extension("bokeh")
 
-# tiles = hv.Tiles('https://tile.openstreetmap.org/{Z}/{X}/{Y}.png', name="OSM")#.opts(width=600, height=550)
 
-def plot_true_color_image(items, time, mask_clouds, resolution):
+def normalize_bounds(in_data):
+    y0 = in_data["y"].min()
+    y1 = in_data["y"].max()
+    dy = y1 - y0
+    x0 = in_data["x"].min()
+    x1 = in_data["x"].max()
+    dx = x1 - x0
+    return int(dy/dx)
+
+
+def plot_true_color_image(items, time, resolution):
     """
     A function that plots the True Color band combination.
     """
@@ -52,25 +49,25 @@ def plot_true_color_image(items, time, mask_clouds, resolution):
                 tool.zoom_on_axis = False
                 break
 
-
     print(f"loading data & generating RGB plot for {str(time)}")
-    #TODO: add time downselection
 
-    mask = [i.datetime.date() == time for i in items]
-    items = [b for a, b in zip(mask, items) if a]
+    # TODO: add time downselection
+
+    # Get the selected image
+    sel_item = [it for it in items if it.datetime.date() == time]
 
     # aws_session = AWSSession(requester_pays=True)
     # with rio.Env(aws_session):
+
     print("loading delayed data")
     s2_data = stac_load(
-        items,
-        bands=["red", "green", "blue"],#, "nir", "nir08", "swir16", "swir22"],
+        sel_item,
+        bands=["red", "green", "blue"],  # , "nir", "nir08", "swir16", "swir22"],
         resolution=resolution,
-        chunks={'time':1, 'x': 2048, 'y': 2048},
-        # crs='EPSG:4326',
+        chunks={'time': 1, 'x': 2048, 'y': 2048},
+        # crs='EPSG:3857',  # We need the data in EPSG:3857
         ).to_stacked_array(new_dim='band', sample_dims=('time', 'x', 'y'))
-    
-    s2_data = s2_data.astype("int16")
+
     # Get the selected image and band combination
     # TODO: add stac_load here
     out_data = s2_data.sel(band=[("red",), ("green",), ("blue",)], time=slice(time, time + datetime.timedelta(days=1)))#.median('time')
@@ -86,63 +83,48 @@ def plot_true_color_image(items, time, mask_clouds, resolution):
     # Contrast stretching
     out_data.data = s2_contrast_stretch(out_data.data)
 
-    # Check whether to apply a mask to the image
-    if mask_clouds:
-        # Assign a value of 255 to the pixels representing clouds
-        out_data = out_data.where(out_data.mask == 0, 255)
+    # normalize bounds
+    dxdy = normalize_bounds(out_data)
 
     # Image bands to be plotted
     b0 = out_data.sel(band=("red",)).data
     b1 = out_data.sel(band=("green",)).data
     b2 = out_data.sel(band=("blue",)).data
 
-    # Create masked arrays
-    b0_mask = np.ma.masked_where(b0 == 255, b0)
-    b1_mask = np.ma.masked_where(b1 == 255, b1)
-    b2_mask = np.ma.masked_where(b2 == 255, b2)
-
     # Plot the RGB image
     plot_data = dict(
         x=out_data["x"],
         y=out_data["y"],
-        r=b0_mask,
-        g=b1_mask,
-        b=b2_mask,
+        r=b0,
+        g=b1,
+        b=b2,
     )
 
-    # normalize bounds
-    y0 = out_data["y"].min()
-    y1 = out_data["y"].max()
-    dy = y1 - y0
-    x0 = out_data["x"].min()
-    x1 = out_data["x"].max()
-    dx = x1 - x0
-
-    the_plot = hv.RGB(
+    rgb_plot = hv.RGB(
         data=plot_data,
         kdims=["x", "y"],
-        # bounds=(x0,y0,x1,y1),
+        # bounds=(x0, y0, x1, y1),
         vdims=list("rgb"),
     ).opts(
         xlabel="",
         ylabel="",
         hooks=[hook],
         frame_width=500,
-        frame_height=int(500*dy/dx),
+        frame_height=500 * dxdy,
     )
     print("finished plotting")
-    return the_plot#*tiles
-
-def assign_spindex_to_cache(s2_spindex_name, spindex):
-    """
-    This function assign the spectral index array to the panel state cache
-    so that it can be used for the histogram floatpanel widget.
-    """
-    pn.state.cache["spindex"] = {"name": s2_spindex_name, "np_array": spindex}
+    return rgb_plot  # hv.element.tiles.OSM()
 
 
-# TODO: generalize with spyndex(!)
-def plot_s2_spindex(items, time, s2_spindex, mask_clouds, resolution):
+# def assign_spindex_to_cache(s2_spindex_name, spindex):
+#     """
+#     This function assign the spectral index array to the panel state cache
+#     so that it can be used for the histogram floatpanel widget.
+#     """
+#     pn.state.cache["spindex"] = {"name": s2_spindex_name, "np_array": spindex}
+
+
+def plot_s2_spindex(items, time, s2_spindex, resolution, cmap):
     """
     A function that plots the selected Sentinel-2 spectral index.
     """
@@ -164,41 +146,36 @@ def plot_s2_spindex(items, time, s2_spindex, mask_clouds, resolution):
                 tool.zoom_on_axis = False
                 break
 
-    # TODO: Clean up spyndex use w constants module.
-    spyndex_band_mapping = {
-        "N": ("nir",),
-        "N2": ("nir08",),
-        "R": ("red",),
-        "G": ("green",),
-        "S1": ("swir16",),
-        "S2": ("swir22",),
-        # "L": 0.5
-    }
+    # Get index information
+    s2_index = get_index_props(s2_spindex)
+
     # Get the name of the selected spectral index
-    s2_spindex = S2_SPINDICES[s2_spindex]
-    s2_spindex_name = s2_spindex["name"]
+    s2_spindex_name = s2_index["short_name"]
 
-    load_bands = [spyndex_band_mapping[b] for b in spyndex.indices[s2_spindex['name']].bands]
-    print(f"loading data & generating {s2_spindex['name']} plot for {str(time)}")
+    print(f"loading data & generating {s2_spindex_name} plot for {str(time)}")
 
-    # Get the selected image and band combination
-    mask = [i.datetime.date() == time for i in items]
-    items = [b for a, b in zip(mask, items) if a]
+    # Get the selected image
+    sel_item = [it for it in items if it.datetime.date() == time]
 
     # aws_session = AWSSession(requester_pays=True)
     # with rio.Env(aws_session):
+
     print("loading delayed data")
+
     s2_data = stac_load(
-        items,
-        bands=[b[0] for b in load_bands],
+        sel_item,
+        bands=[b[0] for b in s2_index["stac_bands"]],
         resolution=resolution,
-        chunks={'time':1, 'x': 2048, 'y': 2048},
-        # crs='EPSG:4326',
+        chunks={'time': 1, 'x': 2048, 'y': 2048},
+        # crs="EPSG:3857"
         ).to_stacked_array(new_dim='band', sample_dims=('time', 'x', 'y'))
-    s2_data = s2_data.astype("int16")
+
+    # Image DN to Reflectance (0, 1) and clip between 0, 1
+    s2_data = s2_data / 1e4
+    s2_data = s2_data.clip(0.0, 1.0)
 
     # Get the selected image and band combination
-    out_data = s2_data.sel(time=slice(time, time + datetime.timedelta(days=1)))#.median('time')
+    out_data = s2_data.sel(time=slice(time, time + datetime.timedelta(days=1)))  # .median('time')
 
     # spatial merge
     out_data = merge_arrays(
@@ -207,51 +184,34 @@ def plot_s2_spindex(items, time, s2_spindex, mask_clouds, resolution):
 
     # Define a custom Hover tool for the image
     spindex_hover = HoverTool(
-        tooltips=[(f"{s2_spindex_name}", "@image")],
-        formatters={"@image": HIDE_NAN_HOVTOOL},
+        tooltips=[(f"{s2_spindex_name}", "@image")]
     )
 
-    for b in spyndex_band_mapping:
-        band = spyndex_band_mapping[b]
-        if band in load_bands:
-            spyndex_band_mapping[b] = out_data.sel(band=band)
+    # Get index parameters for spyndex
+    plot_data = compute_index(out_data, s2_index)
 
-    plot_data = spyndex.computeIndex(
-        index = [s2_spindex_name],
-        params = spyndex_band_mapping
-    )
-
-    # Check whether to apply a mask to the image
-    if mask_clouds:
-        # Assign a value of 255 to the pixels representing clouds
-        plot_data[out_data.mask == 1] = 255
-
-    # Create a masked array
-    plot_data_mask = np.ma.masked_where(plot_data == 255, plot_data)
+    # Mask nodata from visualization?
+    plot_data = xr.where(plot_data == 0, 1, plot_data.data)
 
     # Assign this array to the pn.cache
-    assign_spindex_to_cache(s2_spindex_name, plot_data_mask)
+    # assign_spindex_to_cache(s2_spindex_name, plot_data_mask)
 
     # Enable the refresh button of the histogram plot
-    enable_hist_refresh_bt()
+    # enable_hist_refresh_bt()
 
     # normalize bounds
-    y0 = out_data["y"].min()
-    y1 = out_data["y"].max()
-    dy = y1 - y0
-    x0 = out_data["x"].min()
-    x1 = out_data["x"].max()
-    dx = x1 - x0
-    
+    dxdy = normalize_bounds(out_data)
+
     # Plot the computed spectral index
-    the_plot = hv.Image((out_data["x"], out_data["y"], plot_data_mask)).opts(
+    the_plot = hv.Image((plot_data["x"], plot_data["y"], plot_data)).opts(
         xlabel="",
         ylabel="",
-        cmap=s2_spindex["cmap"],
+        cmap=cmap,
+        cnorm="eq_hist",  # Temporary hack: some indices (e.g. SAVI2) have values outside the index range (-1, +1)
         hooks=[hook],
         tools=[spindex_hover],
         frame_width=500,
-        frame_height=int(500*dy/dx),
+        frame_height=500 * dxdy,
     )
     print("finished plotting")
-    return the_plot#*tiles
+    return the_plot
