6bdaba78280af8ca141d6a252d36bc317ec1053d
Kevin Lalli <klalli86@gmail.com>
2023-06-24 21:38:38 +0000

    black/ flake

--- modules/chat_tools.py
+++ modules/chat_tools.py
@@ -1,8 +1,7 @@
-from typing import Tuple, Optional, List, Dict
+from typing import Optional
 import panel as pn
 import param
 from pystac_client.client import Client
-import json
 import geopandas as gpd
 from shapely.geometry import shape, Polygon
 from odc.stac import stac_load
@@ -26,6 +25,7 @@ import pandas as pd
 from datetime import datetime
 from holoviews.operation.datashader import rasterize
 
+
 # TODO: move these back to some module
 def s2_contrast_stretch(in_data):
     """
@@ -37,6 +37,7 @@ def s2_contrast_stretch(in_data):
     in_data.values = exposure.rescale_intensity(in_data, in_range=(p2, p98))
 
     return in_data
+
 
 def s2_image_to_uint8(in_data):
     """
@@ -52,6 +53,7 @@ def s2_image_to_uint8(in_data):
 
     return out_data
 
+
 class MapManager(param.Parameterized):
     ## TODO: use another gdf for zonal stats - items get stored here for plotting
     gdf = param.DataFrame(
@@ -60,21 +62,21 @@ class MapManager(param.Parameterized):
     )
 
     ## STAC search
-    bbox = param.String() # (=seattle)
+    bbox = param.String()  # (=seattle)
     # toi = # (now minus 1-2 months)
     # url =
     # collection(s) = # (satellites)
     items_dict = param.Dict({})
 
     ## Basic view
-    # datacube = 
+    # datacube =
     # band = 'RGB'
-    # available_dates = 
+    # available_dates =
     # selected_date(s) =
-    tile_url = param.String('https://tile.openstreetmap.org/{Z}/{X}/{Y}.png')
+    tile_url = param.String("https://tile.openstreetmap.org/{Z}/{X}/{Y}.png")
     # map_bounds =
-    # mask_clouds = 
-    # clip_range = 
+    # mask_clouds =
+    # clip_range =
     # cmap =  # (if not RGB)
 
     ## Split view
@@ -82,41 +84,36 @@ class MapManager(param.Parameterized):
     # split_band = 'NDVI'
 
     ## Resampling
-    # max_resolution = 
-    # resample_period = 
-    # 
+    # max_resolution =
+    # resample_period =
+    #
 
     ## TODO: decide if we should use
     # def panel(self):
     #     return pn.Column(pn.panel(self._map))
 
-
     def stac_search(
-            self,
-            bbox: str,
-            dtime: str,
-            url: Optional[str] = 'https://earth-search.aws.element84.com/v1/',
-            collections: Optional[list] = ['sentinel-2-l2a'],
-        ) -> str:
+        self,
+        bbox: str,
+        dtime: str,
+        url: Optional[str] = "https://earth-search.aws.element84.com/v1/",
+        collections: Optional[list] = ["sentinel-2-l2a"],
+    ) -> str:
         """Perform a STAC search."""
-        
-        self.bbox = bbox # TODO: change to tuple?
+
+        self.bbox = bbox  # TODO: change to tuple?
 
         client = Client.open(url)
 
-        result = client.search(
-            collections=[collections],
-            bbox=bbox,
-            datetime=dtime
-        )
+        result = client.search(collections=[collections], bbox=bbox, datetime=dtime)
 
         items_dict = result.get_all_items_as_dict()
         self.items_dict = items_dict
         self.gdf = gpd.GeoDataFrame.from_features(items_dict)
 
         return {
-            'count': result.matched(),
-            }
+            "count": result.matched(),
+        }
 
     def view_footprints(
         self,
@@ -125,69 +122,80 @@ class MapManager(param.Parameterized):
     ):
         """Load Sentinel & Landsat STAC item footprints to a map. Not for images. DO NOT use for Aqua/Terra/MODIS."""
 
-        m = self.gdf.loc[:, ['geometry']].set_crs('epsg:4326').explore(tiles="CartoDB positron") # TODO: basemap updates
+        m = (
+            self.gdf.loc[:, ["geometry"]]
+            .set_crs("epsg:4326")
+            .explore(tiles="CartoDB positron")
+        )  # TODO: basemap updates
 
-        chat_box.append(
-            {"SatGPT": pn.pane.plot.Folium(m, height=400)}
-            )
+        chat_box.append({"SatGPT": pn.pane.plot.Folium(m, height=400)})
         return "Map is loaded to chat. Return nothing but a text confirmation to let the user know."
 
     def plot_item_metadata(
         self,
-        field: str = 'eo:cloud_cover',
+        field: str = "eo:cloud_cover",
     ):
         """Plot any field from the current STAC items, e.g. cloud cover. No images, just STAC metadata fields."""
-        dates = [ds.split('T')[0] for ds in self.gdf.loc[:, ['datetime']].values.flatten()]
-        dts = [datetime.strptime(d, '%Y-%m-%d') for d in dates]
-        self.gdf.loc[:, ['date']] = dts
+        dates = [
+            ds.split("T")[0] for ds in self.gdf.loc[:, ["datetime"]].values.flatten()
+        ]
+        dts = [datetime.strptime(d, "%Y-%m-%d") for d in dates]
+        self.gdf.loc[:, ["date"]] = dts
 
         chat_box.append(
-            {"SatGPT": pn.panel(self.gdf.loc[:, ['date', 'eo:cloud_cover']].set_index('date').plot())}
-            )
-        return "Plot is loaded to chat. Return nothing other than 'Plotted!' to the user."
+            {
+                "SatGPT": pn.panel(
+                    self.gdf.loc[:, ["date", "eo:cloud_cover"]].set_index("date").plot()
+                )
+            }
+        )
+        return (
+            "Plot is loaded to chat. Return nothing other than 'Plotted!' to the user."
+        )
 
     def set_basemap(
-        self,
-        datestring: str = '2023-06-09',
-        source: Optional[str] = 'Aqua'
+        self, datestring: str = "2023-06-09", source: Optional[str] = "Aqua"
     ):
         """
-        Sets basemap with Modis (source = 'Aqua' or 'Terra') world coverage by date. 
+        Sets basemap with Modis (source = 'Aqua' or 'Terra') world coverage by date.
         This tool does NOT require a prior STAC search. Currently only supports RGB views, no spectral indices.
         """
-        # Valid: 
+        # Valid:
         # https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/wmts.cgi
         # ?Service=WMTS&Request=GetTile&Version=1.0.0&layer=MODIS_Terra_CorrectedReflectance_TrueColor&tilematrixset=250m
         # &TileMatrix=6&TileCol=36&TileRow=13&TIME=2012-07-09&style=default&Format=image%2Fjpeg
 
-        tileMatrixSet='GoogleMapsCompatible_Level9'
-        layer=f'MODIS_{source}_CorrectedReflectance_TrueColor'
-        base_url = 'https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/'
-        tile_path = f'{layer}/default/{datestring}/{tileMatrixSet}/' + '{Z}/{Y}/{X}.jpg'
+        tileMatrixSet = "GoogleMapsCompatible_Level9"
+        layer = f"MODIS_{source}_CorrectedReflectance_TrueColor"
+        base_url = "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/"
+        tile_path = f"{layer}/default/{datestring}/{tileMatrixSet}/" + "{Z}/{Y}/{X}.jpg"
         self.tile_url = base_url + tile_path
         return "Basemap is set. Return nothing but a text confirmation to let the user know."
-
 
     def show_datacube(self):
         """Display the datacube viewer for the current items (images). Currently only supports RGB views, no spectral indices."""
 
         rgb = self.create_rgb_viewer()
         chat_box.append({"SatGPT": pn.panel(rgb)})
-        
-        return "Datacube is loaded to chat. Return nothing other than 'Done!' to the user."
+
+        return (
+            "Datacube is loaded to chat. Return nothing other than 'Done!' to the user."
+        )
 
     # TODO: make private or move to module
     def s2_hv_plot(
-        self, items, time, type="RGB", 
-        ):
-
+        self,
+        items,
+        time,
+        type="RGB",
+    ):
         TILES = hv.Tiles(self.tile_url)
         # TILES = gv.WMTS(WMTSTileSource(url=url)) # maybe?
 
         mask = [i.datetime.date() == time for i in items]
         items = [b for a, b in zip(mask, items) if a]
-        bbox = tuple(map(float, self.bbox.split(',')))
-        
+        bbox = tuple(map(float, self.bbox.split(",")))
+
         s2_data = stac_load(
             items,
             bbox=bbox,
@@ -200,7 +208,7 @@ class MapManager(param.Parameterized):
         # TODO: add spatial merge back here
         out_data = s2_data.isel(time=0).to_array(dim="band")
 
-        if type == 'RGB':
+        if type == "RGB":
             # RGB data
             rgb_data = out_data.sel(band=["red", "green", "blue"])
 
@@ -211,36 +219,35 @@ class MapManager(param.Parameterized):
             rgb_data = s2_contrast_stretch(rgb_data)
 
             rgb_plot = rgb_data.hvplot.rgb(
-                    x="x",
-                    y="y",
-                    bands='band',
-                    frame_height=500,
-                    frame_width=500,
-                    xaxis=None,
-                    yaxis=None
-                    )  # .redim.nodata(value=0)
+                x="x",
+                y="y",
+                bands="band",
+                frame_height=500,
+                frame_width=500,
+                xaxis=None,
+                yaxis=None,
+            )  # .redim.nodata(value=0)
 
             # This is working with swipe and hvplot
             rgb_plot = TILES * rasterize(rgb_plot, expand=False)
 
-            return(rgb_plot)
-            
+            return rgb_plot
+
     def create_rgb_viewer(self):
-        
-        items = pystac.ItemCollection(self.items_dict['features'])
-        
+        items = pystac.ItemCollection(self.items_dict["features"])
+
         # Time variable
         time_var = [i.datetime for i in items]
         time_date = [t.date() for t in time_var]
 
         time_select = pn.widgets.DatePicker(
             name="Date",
-            value=time_date[0], 
-            start=time_date[-1], 
-            end=time_date[0], 
+            value=time_date[0],
+            start=time_date[-1],
+            end=time_date[0],
             enabled_dates=time_date,
-            description="Select the date for plotting."
-            )
+            description="Select the date for plotting.",
+        )
 
         s2_true_color_bind = pn.bind(
             self.s2_hv_plot,
@@ -254,29 +261,23 @@ class MapManager(param.Parameterized):
         return pn.Column(time_select, s2_true_color_bind)
 
 
-
-
-
-
-
-
 map_mgr = MapManager()
 
 # define tools
 # tools == a wrapped method above
 search_tool = StructuredTool.from_function(map_mgr.stac_search)
 gribs_tool = StructuredTool.from_function(map_mgr.set_basemap)
-datacube_tool  = StructuredTool.from_function(map_mgr.show_datacube)
+datacube_tool = StructuredTool.from_function(map_mgr.show_datacube)
 plot_tool = StructuredTool.from_function(map_mgr.plot_item_metadata)
 map_tool = StructuredTool.from_function(map_mgr.view_footprints)
 
 tools = [
-    search_tool, 
-    map_tool, 
+    search_tool,
+    map_tool,
     # gribs_tool, # not working yet
     plot_tool,
-    datacube_tool
-    ]
+    datacube_tool,
+]
 
 
 # chatbox component needs to be here due to how we add content above
